2a433239ece23a4e9dc435a3338f46f277d6f7b0
SmaHonda <86571238+SmaHonda@users.noreply.github.com>
2021-08-23 17:22:19 +0800

    0823

--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# hello
-
-THIS IS MADE BY VUEPRESS
---
+++ vuepress-starter/docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    base: '/project01/',
+    themeConfig: {
+        sidebarDepth: 5,
+        sidebar: [
+          ['/', 'info']
+        ],
+        nav: [
+          { text: 'Home', link: '/' },
+        ]
+      }
+  }
